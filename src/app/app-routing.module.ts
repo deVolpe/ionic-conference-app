39f47b9ab5efbf0dd21baf2cdf9329b0a7654c8f
Vladimir <successcraftlabtasks@gmail.com>
2019-10-02 23:04:50 +0300

    add order form tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,28 +10,38 @@ const routes: Routes = [
   },
   {
     path: 'account',
-    loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
+    loadChildren: () =>
+      import('./pages/account/account.module').then(m => m.AccountModule)
   },
   {
     path: 'support',
-    loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule)
+    loadChildren: () =>
+      import('./pages/support/support.module').then(m => m.SupportModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then(m => m.LoginModule)
   },
   {
     path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignUpModule)
+    loadChildren: () =>
+      import('./pages/signup/signup.module').then(m => m.SignUpModule)
   },
   {
     path: 'app',
-    loadChildren: () => import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
+    loadChildren: () =>
+      import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
   },
   {
     path: 'tutorial',
-    loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
+    loadChildren: () =>
+      import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
     canLoad: [CheckTutorial]
+  },
+  {
+    path: 'order-form',
+    loadChildren: './order-form/order-form.module#OrderFormPageModule'
   }
 ];
 
