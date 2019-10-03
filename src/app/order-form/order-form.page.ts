5c9f23dacd82160a0f593c2d24aa58115558d101
Vladimir <successcraftlabtasks@gmail.com>
2019-10-04 01:36:19 +0300

    big impovements

--- src/app/order-form/order-form.page.ts
+++ src/app/order-form/order-form.page.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+
+import { ReceiverOptions, ShippingOptions } from '../interfaces/order-options';
 
 @Component({
   selector: 'order-form',
   templateUrl: './order-form.page.html',
-  styleUrls: ['./order-form.page.scss'],
+  styleUrls: ['./order-form.page.scss']
 })
 export class OrderFormPage implements OnInit {
+  receiver: ReceiverOptions = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: ''
+  };
 
-  constructor() { }
+  shipping: ShippingOptions = {
+    address: '',
+    city: '',
+    country: 'United States',
+    state: '',
+    postalCode: null
+  };
 
-  ngOnInit() {
+  isSubmitted = false;
+
+  constructor(private toastController: ToastController) {}
+
+  ngOnInit() {}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Success',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
   }
 
+  async onAddPaymentInfo(form: NgForm) {
+    this.isSubmitted = true;
+
+    if (form.valid) {
+      await this.presentToast();
+    }
+  }
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -6,21 +6,16 @@ import { UserData } from '../../providers/user-data';
 
 import { UserOptions } from '../../interfaces/user-options';
 
-
-
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
-  styleUrls: ['./login.scss'],
+  styleUrls: ['./login.scss']
 })
 export class LoginPage {
   login: UserOptions = { username: '', password: '' };
   submitted = false;
 
-  constructor(
-    public userData: UserData,
-    public router: Router
-  ) { }
+  constructor(public userData: UserData, public router: Router) {}
 
   onLogin(form: NgForm) {
     this.submitted = true;
